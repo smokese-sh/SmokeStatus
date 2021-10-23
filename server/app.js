d830136037b56819ba740ea65365435577ea1f15
LandenStephenss <turtlederp806@icloud.com>
2021-10-23 01:32:19 -0500

    Fixed a few bugs with the 15 minute thing, added endpoint

--- server/app.js
+++ server/app.js
@@ -3,17 +3,22 @@ const app = express();
 const config = require("./config.json");
 const { CronJob } = require("cron");
 const https = require("https");
+const { MongoClient } = require("mongodb");
 
 class SmokeStatus {
   constructor() {
     this.app = app;
-    // this.loadEndpoints();
+    new MongoClient(config.mongoUri)
+      .connect()
+      .then((res) => res.db("sesh-beta"))
+      .then((db) => {
+        this.collection = db.collection("stats");
+      });
+
+    this.loadEndpoints();
     app.listen(80);
-
-    this.testEndpoints();
-
     new CronJob(
-      "*/15 * * * * *",
+      "0 */15 * * * *",
       () => {
         this.testEndpoints();
       },
@@ -25,8 +30,11 @@ class SmokeStatus {
 
   loadEndpoints() {
     const endpoints = require("./endpoints");
-    for (const endpoint in endpoints) {
-      console.log(endpoints[endpoint]);
+    for (let endpoint in endpoints) {
+      endpoint = endpoints[endpoint];
+      const Ep = new endpoint(this);
+      this.app.get(Ep.url, async (req, res) => await Ep.run(req, res))
+
     }
   }
 
@@ -40,33 +48,32 @@ class SmokeStatus {
           const chunks = [];
           res.on("data", (chunk) => chunks.push(chunk));
           res.on("end", () => {
-            resolve(chunks)
+            resolve(chunks);
           });
         });
-      })
+      });
     };
 
     for (let test of config.tests) {
       const start = Date.now();
       const url = test.startsWith("https") ? test : `${config.domain}${test}`;
       await request(url);
-      tests.push({ url, time: Date.now() - start });
+      tests.push({ url, resTime: Date.now() - start, time: start });
     }
     console.log(tests);
-    // for (let test in config.tests) {
-    //   test = config.tests[test];
-    //   const startTime = process.hrtime();
-    //   const url = test.startsWith("https") ? test : `${config.domain}${test}`;
-    //   const request = https.get(url);
-    //   request.on("response", (res) => {
-    //     let chunks = "";
-    //     res.on("data", (chunk) => (chunks += chunk));
-    //     res.on("end", () => {
-    //       const time = process.hrtime(startTime)[1] / 1000000;
-    //       tests.push({url, time});
-    //     });
-    //   });
-    // }
+
+    let average = 0;
+    for (let { resTime } of tests) {
+      average += resTime;
+    }
+    average = Math.round(average / tests.length);
+
+
+    this.collection.insertOne({
+        _id: tests[0].time,
+        averageRes: average,
+        tests: tests
+    })
   }
 
   testSite() {}
---
+++ server/endpoints/getAverage.js
@@ -0,0 +1,26 @@
+module.exports = class getAverage {
+  constructor(app) {
+    this.app = app;
+    this.url = "/api/v1/getAverage";
+  }
+
+  async run(req, res) {
+    const array = await this.app.collection.find().toArray();
+    let query = req.query?.span;
+    switch (query) {
+      case "day":
+        array.filter((data) => new Date(data).toDateString() === new Date().toDateString());
+        res.json({ data: array.map(data => ({time: data._id, average: data.averageRes})) });
+        break;
+      case "month":
+        break;
+      case "year":
+        break;
+      default:
+        // returns day
+        array.filter((data) => new Date(data).toDateString() === new Date().toDateString());
+        res.json({ data: array });
+        break;
+    }
+  }
+};
